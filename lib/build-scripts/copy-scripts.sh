82d31560bc91d1ccfd551906dc23a4355f3a444b
Daniel Lindsley <daniel@toastdriven.com>
2011-08-17 01:50:08 -0500

    Initial Xcode 4 template support.
    To get it working, run ``rake install``. Then start a new project with the Wax choice. Click the project file at left, then go to "Build Settings". You'll need to manually set the "Product Name". Finally, at a shell, symlink the source ``wax`` checkout into the project.
    
    It should mostly build, dying with something like "ld: symbol(s) not found for architecture i386".

--- lib/build-scripts/copy-scripts.sh
+++ lib/build-scripts/copy-scripts.sh
@@ -3,7 +3,7 @@
 # Created by Corey Johnson
 
 WAX_SCRIPTS_DIR="scripts"
-SOURCE_SCRIPTS_DIR="$PROJECT_DIR/$WAX_SCRIPTS_DIR"
+SOURCE_SCRIPTS_DIR="$PROJECT_DIR/$PROJECT_NAME/$WAX_SCRIPTS_DIR"
 DESTINATION_SCRIPTS_DIR="$BUILT_PRODUCTS_DIR/$CONTENTS_FOLDER_PATH/$WAX_SCRIPTS_DIR"
 
 # Verify that the user isn't using an old version of Wax
@@ -14,7 +14,7 @@ if [ -d "$PROJECT_DIR/data/scripts" ]; then
 fi
 
 # If the user has luac installed, preparse the lua files and make sure they are cool
-if type luac &> /dev/null; then; 
+if type luac &> /dev/null; then;
   LUAC_OUTPUT=$(luac -p -- "$SOURCE_SCRIPTS_DIR"/**/*.lua 2>&1)
   LUAC_EXIT_VALUE=$?
   if [ $LUAC_EXIT_VALUE != 0 ] ; then
@@ -32,16 +32,16 @@ mkdir -p "$DESTINATION_SCRIPTS_DIR"
 
 if [ $WAX_COMPILE_SCRIPTS ]; then
   echo "note: Wax is using compiled Lua scripts."
-  # This requires that you run a special build of lua. Since snow leopard is 64-bit 
+  # This requires that you run a special build of lua. Since snow leopard is 64-bit
   # and iOS is 32-bit, luac files compiled on snow leopard won't work on iOS
-  
-  lua "$PROJECT_DIR/wax/lib/build-scripts/luac.lua" wax wax.dat "$PROJECT_DIR/wax/lib/stdlib/" "$PROJECT_DIR/wax/lib/stdlib/init.lua" -L "$PROJECT_DIR/wax/lib/stdlib"/**/*.lua
+
+  lua "$PROJECT_DIR/$PROJECT_NAME/wax/lib/build-scripts/luac.lua" wax wax.dat "$PROJECT_DIR/$PROJECT_NAME/wax/lib/stdlib/" "$PROJECT_DIR/$PROJECT_NAME/wax/lib/stdlib/init.lua" -L "$PROJECT_DIR/$PROJECT_NAME/wax/lib/stdlib"/**/*.lua
   lua "$PROJECT_DIR/wax/lib/build-scripts/luac.lua" "" AppDelegate.dat "$SOURCE_SCRIPTS_DIR/" "$SOURCE_SCRIPTS_DIR/AppDelegate.lua" -L "$SOURCE_SCRIPTS_DIR"/**/*.lua
   mv AppDelegate.dat "$DESTINATION_SCRIPTS_DIR"
   mv wax.dat "$DESTINATION_SCRIPTS_DIR"
 else
   # copy everything in the data dir to the app (doesn't just have to be lua files, can be images, sounds, etc...)
-  cp -r "$PROJECT_DIR/wax/lib/stdlib" "$DESTINATION_SCRIPTS_DIR/wax"
+  cp -r "$PROJECT_DIR/$PROJECT_NAME/wax/lib/stdlib" "$DESTINATION_SCRIPTS_DIR/wax"
   cp -r "$SOURCE_SCRIPTS_DIR/" "$DESTINATION_SCRIPTS_DIR"
 fi
 
